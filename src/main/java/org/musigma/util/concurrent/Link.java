4d83532e42884d42b84f245a34857fcc30aa2296
Matt Sicker <boards@gmail.com>
2019-06-18 22:49:00 -0500

    Unroll tail recursion
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- src/main/java/org/musigma/util/concurrent/Link.java
+++ src/main/java/org/musigma/util/concurrent/Link.java
@@ -11,25 +11,24 @@ class Link<T> {
         ref = new AtomicReference<>(to);
     }
 
-    DefaultPromise<T> promise(final DefaultPromise<T> owner) {
-        final DefaultPromise<T> current = ref.get();
-        return compressed(current, current, owner);
-    }
-
     @SuppressWarnings("unchecked")
-    private DefaultPromise<T> compressed(final DefaultPromise<T> current, final DefaultPromise<T> target, final DefaultPromise<T> owner) {
-        final Object value = target.ref.get();
-        if (value instanceof Callbacks) {
-            if (ref.compareAndSet(current, target)) {
-                return target; // linked
+    DefaultPromise<T> promise(final DefaultPromise<T> owner) {
+        DefaultPromise<T> current = ref.get();
+        DefaultPromise<T> target = current;
+        while (true) {
+            final Object value = target.ref.get();
+            if (value instanceof Callbacks) {
+                if (ref.compareAndSet(current, target)) {
+                    return target; // linked
+                } else {
+                    current = ref.get();
+                }
+            } else if (value instanceof Link) {
+                target = ((Link<T>) value).ref.get();
             } else {
-                return compressed(ref.get(), target, owner); // retry
+                owner.unlink((Thunk<T>) value);
+                return owner;
             }
-        } else if (value instanceof Link) {
-            return compressed(current, ((Link<T>) value).ref.get(), owner);
-        } else {
-            owner.unlink((Thunk<T>) value);
-            return owner;
         }
     }
 }
