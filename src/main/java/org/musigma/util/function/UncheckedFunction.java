6213cf3a2ce017ae9fe1b5cfa7dcc1b2dd4a366a
Matt Sicker <boards@gmail.com>
2019-06-22 23:19:15 -0500

    Add identity function
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- src/main/java/org/musigma/util/function/UncheckedFunction.java
+++ src/main/java/org/musigma/util/function/UncheckedFunction.java
@@ -2,5 +2,9 @@ package org.musigma.util.function;
 
 @FunctionalInterface
 public interface UncheckedFunction<T, R> {
+    static <T> UncheckedFunction<T, T> identity() {
+        return t -> t;
+    }
+
     R apply(final T value) throws Throwable;
 }
