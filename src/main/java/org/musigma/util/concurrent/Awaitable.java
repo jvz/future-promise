d2ec2c88d54adb026ba717504781d909badf3cb2
Matt Sicker <boards@gmail.com>
2019-06-30 18:29:58 -0500

    Add await static methods
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- src/main/java/org/musigma/util/concurrent/Awaitable.java
+++ src/main/java/org/musigma/util/concurrent/Awaitable.java
@@ -4,7 +4,19 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public interface Awaitable {
+
     void await() throws InterruptedException;
 
     void await(final long time, final TimeUnit unit) throws InterruptedException, TimeoutException;
+
+    static <F extends Awaitable> F await(final F future) throws InterruptedException {
+        future.await();
+        return future;
+    }
+
+    static <F extends Awaitable> F await(final F future, final long time, final TimeUnit unit) throws TimeoutException, InterruptedException {
+        future.await(time, unit);
+        return future;
+    }
+
 }
