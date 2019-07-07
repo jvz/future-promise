876f6936970fb016f4d494c80ae4d95280a06bf0
Matt Sicker <boards@gmail.com>
2019-07-07 16:17:43 -0500

    Add equals/hashCode/toString to Pair
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- core/src/main/java/org/musigma/util/Pair.java
+++ core/src/main/java/org/musigma/util/Pair.java
@@ -1,6 +1,7 @@
 package org.musigma.util;
 
 import java.util.Map;
+import java.util.Objects;
 
 public final class Pair<A, B> implements Map.Entry<A, B> {
 
@@ -12,8 +13,18 @@ public final class Pair<A, B> implements Map.Entry<A, B> {
         this.right = right;
     }
 
+    /**
+     * Constructs an immutable ordered pair with the given values.
+     */
     public static <A, B> Pair<A, B> of(final A left, final B right) {
         return new Pair<>(left, right);
+    }
+
+    /**
+     * Constructs an immutable ordered pair from an existing Map Entry.
+     */
+    public static <A, B> Pair<A, B> from(final Map.Entry<A, B> entry) {
+        return entry instanceof Pair ? (Pair<A, B>) entry : new Pair<>(entry.getKey(), entry.getValue());
     }
 
     @Override
@@ -38,4 +49,24 @@ public final class Pair<A, B> implements Map.Entry<A, B> {
     public B setValue(final B value) {
         throw new UnsupportedOperationException("immutable");
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(left, pair.left) &&
+                Objects.equals(right, pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" + left + ", " + right + '}';
+    }
+
 }
