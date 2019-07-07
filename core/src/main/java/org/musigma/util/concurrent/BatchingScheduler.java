c84e57005b5f5989170408029b61dc751e01d551
Matt Sicker <boards@gmail.com>
2019-07-07 16:16:31 -0500

    Add note about source
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- core/src/main/java/org/musigma/util/concurrent/BatchingScheduler.java
+++ core/src/main/java/org/musigma/util/concurrent/BatchingScheduler.java
@@ -1,6 +1,8 @@
 package org.musigma.util.concurrent;
 
 interface BatchingScheduler<BatchT extends AbstractBatch> extends Scheduler {
+    // synchronous and asynchronous batching based on:
+    // https://github.com/scala/scala/pull/7663
 
     BatchT getCurrentBatch();
 
