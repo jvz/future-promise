d1d03d1f308fdd9f52d2f9e96429f7423aeb6a12
Matt Sicker <boards@gmail.com>
2019-07-20 12:30:55 -0500

    Reduce scope
    Signed-off-by: Matt Sicker <boards@gmail.com>

--- core/src/main/java/org/musigma/util/concurrent/Batching.java
+++ core/src/main/java/org/musigma/util/concurrent/Batching.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
 import java.util.function.Consumer;
 
-public final class Batching {
+final class Batching {
 
     private Batching() {
     }
